f024a4cb117b0fb38e566566c54880d2de043b5d
AlphaNow <simon.palade37@gmail.com>
2025-05-08 20:00:21 +0200

    Added a svg utility

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+use std::path::PathBuf;
+
+use geometry::VecN;
+use svg::object::Polygon;
+
+pub mod datastructures;
 pub mod geometry;
 pub mod graphs;
-pub mod svg;
 pub mod macros;
-pub mod datastructures;
+pub mod svg;
 
 fn main() {
-    println!("hey !")
+    let out = PathBuf::from("/home/alpha_now/Desktop/progs/tipe_project/out");
+
+    let mut svg = svg::SvgGroup::default();
+    svg.push(
+        Polygon {
+            color: "red".to_string(),
+            points: vec![
+                VecN([10., 30.]),
+                VecN([60., 100.]),
+                VecN([20., -50.]),
+                VecN([25., 10.]),
+            ],
+        },
+        0.,
+    );
+
+    svg.write_to_file(&out.join("test.svg"));
 }

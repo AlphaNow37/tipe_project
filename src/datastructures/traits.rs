bb8789eded46e88c7dd86af1cc59c9ce874d743c
AlphaNow <simon.palade37@gmail.com>
2025-05-08 20:01:07 +0200

    Added not nan f64 fro ord/hash impl

--- src/datastructures/traits.rs
+++ src/datastructures/traits.rs
@@ -6,12 +6,12 @@ use std::{
     ops::{Add, Deref, Mul, Sub},
 };
 
-make_trait_alias!(Weight = [Sized + Zero + Add<Output=Self> + Ord] {});
+make_trait_alias!(Weight = [Sized + Zero + Add<Output=Self> + Ord + Copy] {});
 
 /// A Non-NaN, hashable float
 #[derive(Default, Debug, PartialEq, PartialOrd, Clone, Copy)]
-pub struct NiceF64(f64);
-impl NiceF64 {
+pub struct NotNanF64(f64);
+impl NotNanF64 {
     pub const fn new(v: f64) -> Self {
         if v.is_nan() {
             panic!("Got a NaN float");
@@ -19,38 +19,38 @@ impl NiceF64 {
         Self(v)
     }
 }
-impl Deref for NiceF64 {
+impl Deref for NotNanF64 {
     type Target = f64;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
-impl Eq for NiceF64 {}
-impl Ord for NiceF64 {
+impl Eq for NotNanF64 {}
+impl Ord for NotNanF64 {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.0
             .partial_cmp(&other.0)
             .expect(&format!("Can't compare {} and {}", self.0, other.0))
     }
 }
-impl Hash for NiceF64 {
+impl Hash for NotNanF64 {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         state.write_u64(self.0.to_bits());
     }
 }
-impl Add for NiceF64 {
+impl Add for NotNanF64 {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
         Self(*self + *rhs)
     }
 }
-impl Sub for NiceF64 {
+impl Sub for NotNanF64 {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
         Self(*self - *rhs)
     }
 }
-impl Mul for NiceF64 {
+impl Mul for NotNanF64 {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
         Self(*self * *rhs)

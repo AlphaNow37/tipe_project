d69a9beed02c8fca475a114f4979b1fb15257ce8
AlphaNow <simon.palade37@gmail.com>
2025-07-28 13:35:57 +0200

    Migrated bench.py to csv

--- tools/benchs.py
+++ tools/benchs.py
@@ -1,40 +1,53 @@
 import matplotlib.pyplot as plt
-import json
+import csv
 from math import log
 
-with open("out/perf_benchmark copy.json") as f:
-    datas = json.load(f)
+with open("out/nice_perf_benchmark.csv") as f:
+    reader = csv.DictReader(f, delimiter=";")
+    fields = reader.fieldnames
+    datas = list(reader)
 
-entries = datas["entries"]
-params = datas["params"]
+
+# Configuration
+graphed_items = [
+    "time_naive_full",
+    "time_opt1_full",
+    "time_naive_cache",
+    "time_opt1_cache",
+]
+params = [int(entry["nb_map_vertices"]) for entry in datas]
+
 
 fnaive = (lambda n: n**3, "~n^3")
 fopt1 = (lambda n: (n**2) * log(n), "~n^2 log n")
-
 asymps = {
-    "naive": fnaive,
-    "opt1": fopt1,
-    "naive_full": fnaive,
-    "opt1_full": fopt1,
-    "naive_cache": fnaive,
-    "opt1_cache": fopt1,
+    "time_naive_full": fnaive,
+    "time_opt1_full": fopt1,
+    "time_naive_cache": fnaive,
+    "time_opt1_cache": fopt1,
 }
 colors = {
-    "naive": "red",
-    "opt1": "green",
-    "naive_full": "red",
-    "opt1_full": "green",
-    "naive_cache": "blue",
-    "opt1_cache": "purple",
+    "time_naive_full": "red",
+    "time_opt1_full": "green",
+    "time_naive_cache": "blue",
+    "time_opt1_cache": "purple",
+}
+labels = {
+    "time_naive_full": "Algo naif",
+    "time_opt1_full": "Algo rayon tournant",
+    "time_naive_cache": "Algo naif, lazy",
+    "time_opt1_cache": "ALgo rayon tournant, lazy",
 }
 
-for key, (f, label) in asymps.items():
-    if key not in entries:
+for key in graphed_items:
+    if key not in fields:
         continue
-    times = entries[key]
-    plt.plot(params, times, label=key)
-    vals_asymp = [f(n) * (times[-1]) / f(params[-1]) for n in params]
-    plt.plot(params, vals_asymp, label=label)
+    times = [float(entry[key]) for entry in datas]
+    plt.plot(params, times, label=labels[key], color=colors[key])
+    if key in asymps:
+        (f, label) = asymps[key]
+        vals_asymp = [f(n) * (times[-1]) / f(params[-1]) for n in params]
+        plt.plot(params, vals_asymp, label=label, color=colors[key], linestyle="dashed")
 
 plt.legend()
 plt.show()
